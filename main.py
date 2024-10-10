f3dc4165bcb5a7d1d9227c4a30bcd253835d84af
Carlos <carlossebastian2303@hotmail.com>
2024-10-10 16:38:41 -0400

    Added basic app setup

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-# This is the main file
+from datetime import datetime
+import os
+import logging
+from flask import Flask
+from flask import flash
+from flask import render_template
+from flask import redirect
+from flask import request, session, g, url_for
+from passlib.hash import sha256_crypt
 
-# this is a change to test git functionality
+logging.basicConfig(
+    level=logging.ERROR,
+    format="%(asctime)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="references/info.log"
+)
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+IMG_FOLDER = os.path.join("static", "IMG")
+app.config["UPLOAD_FOLDER"] = IMG_FOLDER
+AVATAR = os.path.join(app.config["UPLOAD_FOLDER"], "avatar.png")
